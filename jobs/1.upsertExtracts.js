81af7abfc9a6fb43937c0b64bd67c1997e1f9aae
mamadou <mamadou.lakhassane@gmail.com>
2021-04-22 12:01:03 +0000

    update mapping with new path, #3

--- jobs/1.upsertExtracts.js
+++ jobs/1.upsertExtracts.js
@@ -17,34 +17,36 @@ alterState(state => {
     service_request_agency: data['systemuser.progres_partner'],
     service_request_phone: data['systemuser.mobilephone'],
     service_request_email: data['systemuser.internalemailaddress'],
-    unhcr_individual_no: data['progres_individual.progres_id'],
-    unhcr_id_no: data['progres_individual.progres_id_registrationgroupid'],
-    name_first: data['progres_individual.progres_givenname'],
-    name_middle: data['progres_individual.progres_middlename'],
-    name_last: data['progres_individual.progres_familyname'],
-    name_nickname: data['progres_individual.progres_id_commonyusedname'],
-    date_of_birth: data['progres_individual.progres_dateofbirth'],
-    age: calculateAge(new Date(data['progres_individual.progres_dateofbirth'])),
-    sex: data['progres_individual.progres_sex'],
+    unhcr_individual_no: data['individuals.progres_id'],
+    unhcr_id_no: data['individuals.progres_id_registrationgroupid'],
+    name_first: data['individuals.progres_givenname'],
+    name_middle: data['individuals.progres_middlename'],
+    name_last: data['individuals.progres_familyname'],
+    name_nickname: data['individuals.progres_id_commonyusedname'],
+    date_of_birth: data['individuals.progres_dateofbirth'].split('T')[0],
+    age: calculateAge(new Date(data['individuals.progres_dateofbirth'])),
+    sex: data['individuals.progres_sex'],
     address_current: '', // advise on mapping
-    telephone_current: data['progres_individual.progres_primaryphonenumber'],
+    telephone_current: data['individuals.progres_primaryphonenumber'],
     protection_concerns: '', // advise on mapping
     language: '', // Field on sheet is multi-value
     service_referral_notes: data.progres_interventionbyother, // Reason for referral ?
     status: '', // advise on mapping
     closure_reason: data.progres_comments_nonrestrictedstore,
     module_id: '', // set on update
-    id: data.progres_primeroid, // Advise on mapping
+    id: data.progres_primeroid || data['individuals.progres_id'], // Advise on mapping
     service_referral_notes: '', // IS PRESENT TWICE IN THE MAPPING SHEET
     created_by: '', // advise on mapping
     created_by_source: '', // advise on mapping
     service_response_type: '', // advise on mapping
   };
 
+  console.log(body);
+
   return getCases(
     {
       remote: true,
-      case_id: data.progres_primeroid || data['progres_individual.progres_id'],
+      case_id: data.progres_primeroid || data['individuals.progres_id'],
     },
     state => {
       console.log(state.data);
