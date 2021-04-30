cba7ab014d871bf1fbe15c1e800e997dd8294539
Aleksa Krolls <aleksagk@gmail.com>
2021-04-30 17:27:54 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/2.a.getCases.js
+++ jobs/2.a.getCases.js
@@ -7,6 +7,7 @@ getCases(
       data =>
         data.services_section &&
         data.services_section[0].service_implementing_agency === 'unhcr'
+        //where last_updated_at = since last run
     );
     
     console.log(cases.length, 'referrals fetched.');
