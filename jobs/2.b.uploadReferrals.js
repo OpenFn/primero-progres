056c1aae5ca8701148bf08a23e823c83c07ffeac
Aleksa Krolls <aleksagk@gmail.com>
2021-05-04 11:39:30 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/2.b.uploadReferrals.js
+++ jobs/2.b.uploadReferrals.js
@@ -10,11 +10,11 @@ each(
     const { urlDTP, key, cert } = configuration;
 
     const obj = {
-      progres_businessunit: '', // inside an array
-      progres_referraldate: '', // inside an array
-      progres_requestedservice: '', // inside an array
+      progres_businessunit: 'd69e8ec1-e80b-e611-80d3-001dd8b71f12', //Dadaab UAT hardcoded GUID for testing
+      progres_referraldate: '2021-04-28T19:34:43.000Z', // inside an array
+      progres_requestedservice: 'documentation', // inside an array
       progres_otherrequestedservices: '', // inside an array
-      progres_reasonforreferral: '', // absent from case
+      progres_reasonforreferral: 'Testing for interoperability', // absent from case
       progres_organizationfrom: data.owned_by_agency_id,
       progres_orgreferredby: data.owned_by, //data.user.full_name
       progres_orgposition: data.user ? data.user.position : 'Case Worker', //TODO: Get user info from endpoint? 
@@ -30,11 +30,10 @@ each(
       progres_pocsex: data.sex,
       progres_pocaddress: data.address_current,
       progres_pocphonenumber: data.telephone_current,
-      progres_spneedcategory: '', // Advise on mapping
+      progres_spneedcategory: 'DS-SC', // Advise on mapping
       progres_otherprotectionconcerns: data.protection_concerns_other,
-      progres_comments: '', // field present multiple times
       progres_primerotransferstatus: '', // inside an array
-      progres_comments: '', // field present multiple times
+      progres_comments: 'English', // field present multiple times
       progres_orgreferralid: data.id,
       progres_priority: 'High and Emergency'
       // progres_priority:
