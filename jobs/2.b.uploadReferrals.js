8e6ea2074ccd9979250a0aa3cc56258e3af43b1d
Aleksa Krolls <aleksagk@gmail.com>
2021-05-04 11:09:09 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/2.b.uploadReferrals.js
+++ jobs/2.b.uploadReferrals.js
@@ -37,7 +37,7 @@ each(
       progres_comments: '', // field present multiple times
       progres_orgreferralid: data.id,
       progres_priority:
-        risk_level === 'High' ? 'High and Emergency' : risk_level,
+        risk_level && risk_level === 'High' ? 'High and Emergency' : risk_level,
     };
     console.log('Mapping referral to DTP:', JSON.stringify(state.data, null, 2));
 
