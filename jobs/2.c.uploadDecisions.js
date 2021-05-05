3f67f975ec8fc28a0228546b5c6b1816e598d54e
aleksa-krolls <aleksagk@gmail.com>
2021-05-06 02:14:10 +0300

    new job 3 and mapping updates

--- jobs/2.c.uploadDecisions.js
+++ jobs/2.c.uploadDecisions.js
@@ -1,19 +1,23 @@
 alterState(state => {
     const { data } = state;
-    const decision = {
-        progres_interventionnumber: data.progres_interventionnumber,
-        service_type: data.progres_interventiontype2,
-        unhcr_individual_no: data['individuals.progres_id'],
-        unhcr_id_no: data['individuals.progres_id_registrationgroupid'],
-        service_referral_notes: data.progres_interventionbyother, // Reason for referral?
-        status: data.progres_primerotransferstatus, //Confirm status conversion mapping
-        closure_reason: data.progres_comments_nonrestrictedstore,
-        service_referral_notes: data.progres_comments_nonrestrictedstore, //repeat mapping?
-        module_id: 'primero-cp', //Confirm module
-        id: data.progres_primeroid || data['individuals.progres_comments'], // Advise on mapping
-    };
+    const interventions = [];
+    interventions.forEach(interventions => {
+        const decision = {
+            progres_interventionnumber: data.progres_interventionnumber,
+            service_type: data.progres_interventiontype2,
+            unhcr_individual_no: data['individuals.progres_id'],
+            unhcr_id_no: data['individuals.progres_id_registrationgroupid'],
+            service_referral_notes: data.progres_interventionbyother, // Reason for referral?
+            status: data.progres_primerotransferstatus, //Confirm status conversion mapping
+            closure_reason: data.progres_comments_nonrestrictedstore,
+            service_referral_notes: data.progres_comments_nonrestrictedstore, //repeat mapping?
+            module_id: 'primero-cp', //Confirm module
+            id: data.progres_primeroid || data['individuals.progres_comments'], // Advise on mapping
+        };
+        interventions.push(decision);
+    });
 
-    console.log('Decision to send back to Primero:', decision);
+    console.log('Decision to send back to Primero:', interventions);
 
     return getCases(
         {
