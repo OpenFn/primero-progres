4317cceb2ba7798c070a0b8034e58f11bdf94631
aleksa-krolls <aleksagk@gmail.com>
2021-05-06 11:30:17 +0300

    to update sendreferral extraction criteria

--- jobs/1.b.sendReferrals.js
+++ jobs/1.b.sendReferrals.js
@@ -1,15 +1,28 @@
 alterState(state => {
   const { data } = state;
-  const { urlDTP, key, cert } = state.configuration;
-  const decision = {
-        case_id: 'b30cba6b-8d97-4524-b77f-a8f50cfcc974',
-        owned_by: 'unhcr_cw',
-        progres_interventionnumber: 'NAI-20-PRTITV-0000006',
-        status: 'rejected',
-        closure_reason: 'primero reason for rejection',
-        request_type: 'Primero Referral Decision'
-      }; 
-  return http
+  getCases(
+    {
+      remote: true,
+      //case_id: data.progres_primeroid || data['individuals.progres_id'], //wrong filter
+      //TODO: get all cases where...
+      //1) data.last_updated_at = Last 24 hours AND data.status !== 'open'
+      //2) data.services_section[...] contains service where progres_interventionnumber!==undefined
+    },
+    state => {
+      const { urlDTP, key, cert } = state.configuration;
+
+
+      const decision = {
+        case_id: data.case_id,
+        owned_by: data.owned_by,
+        progres_interventionnumber: data.progres_interventionnumber,
+        status: data.status,
+        closure_reason: '', // advise on mapping 
+        request_type: 'Primero Referral Decision' //default hardcode
+      };
+      console.log('Decision to send to DTP: ', JSON.stringify(decision, null, 2));
+
+      return http
         .post({
           url: urlDTP,
           data: decision,
@@ -26,48 +39,17 @@ alterState(state => {
           console.log('Decision has been sent.');
           return state;
         });
-    return state;
-}); 
-      
-// alterState(state => {
-//   const { data } = state;
-//   getCases(
-//     {
-//       remote: true,
-//       case_id: data.progres_primeroid || data['individuals.progres_id'],
-//     },
-//     state => {
-//       const { urlDTP, key, cert } = state.configuration;
-    
-     
-//       // const decision = {
-//       //   case_id: data.case_id,
-//       //   owned_by: '', // Advise on mapping
-//       //   progres_interventionnumber: data.progres_interventionnumber,
-//       //   status: data.status,
-//       //   closure_reason: '', // advise on mapping
-//       //   request_type: 'Primero Referral Decision'
-//       // };
-//       console.log('Decision to send to DTP: ', JSON.stringify(decision, null, 2)); 
+    }
+  );
+  return state;
+});
 
-//       return http
-//         .post({
-//           url: urlDTP,
-//           data: decision,
-//           headers: {
-//             'Ocp-Apim-Subscription-Key':
-//               state.configuration['Ocp-Apim-Subscription-Key'],
-//           },
-//           agentOptions: {
-//             key,
-//             cert,
-//           },
-//         })(state)
-//         .then(() => {
-//           console.log('Decision has been sent.');
-//           return state;
-//         });
-//     }
-//   );
-//   return state;
-// });
+//==== Example decision output to post to DTP ===///
+// decision = {
+//   case_id: 'b30cba6b-8d97-4524-b77f-a8f50cfcc974',
+//   owned_by: 'unhcr_cw',
+//   progres_interventionnumber: 'NAI-20-PRTITV-0000006',
+//   status: 'rejected',
+//   closure_reason: 'primero reason for rejection',
+//   request_type: 'Primero Referral Decision'
+// }; 
