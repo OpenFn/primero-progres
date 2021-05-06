0f1aa6edc3949419395c01696da324861ed41df6
mamadou <mamadou.lakhassane@gmail.com>
2021-05-06 11:49:33 +0000

    update mapping for services_section array, #3

--- jobs/1.upsertExtracts.js
+++ jobs/1.upsertExtracts.js
@@ -2,60 +2,58 @@ each(
   dataPath('interventions[*]'),
   alterState(state => {
     const { data } = state;
-    const { interventions } = data;
+
     const calculateAge = dob => {
       const diff = Date.now() - dob.getTime();
       const age_dt = new Date(diff);
 
       return Math.abs(age_dt.getUTCFullYear() - 1970);
     };
-    const referrals = [];
-    interventions.forEach(data => {
-      const obj = {
-        service_response_day_time: data.progres_interventionstartdate,
-        // owned_by: '',
-        progres_interventionnumber: data.progres_interventionnumber,
-        // service_type: data.progres_interventiontype2,
-        service_request_external: '', // No data from progres
-        service_referral_notes: data.progres_comments_nonrestrictedstore,
-        service_request_title: data['systemuser.title'],
-        service_request_agency: data['systemuser.progres_partner'],
-        service_request_phone: data['systemuser.mobilephone'],
-        service_request_email: data['systemuser.internalemailaddress'],
-        service_referral_notes: data.progres_interventionbyother, // Reason for referral ?
-        service_response_type: '', // advise on mapping
-        closure_reason: data.progres_comments_nonrestrictedstore,
-        unhcr_individual_no: data['individuals.progres_id'],
-        unhcr_id_no: data['individuals.progres_id_registrationgroupid'],
-        name_first: data['individuals.progres_givenname'],
-        name_middle: data['individuals.progres_middlename'],
-        name_last: data['individuals.progres_familyname'],
-        name_nickname: data['individuals.progres_id_commonyusedname'],
-        date_of_birth: data['individuals.progres_dateofbirth'].split('T')[0],
-        age: calculateAge(new Date(data['individuals.progres_dateofbirth'])),
-        sex: data['individuals.progres_sex'],
-        address_current: '', // advise on mapping
-        telephone_current: data['individuals.progres_primaryphonenumber'],
-        protection_concerns: '', // advise on mapping
-        language: [], // Field on sheet is multi-value
-        status: 'open', // advise on mapping
-        module_id: 'primero-cp', //hardcode default
-        remote: true, //hardcode default
-        id: data.progres_primeroid || data['individuals.progres_id'], // Advise on mapping
-        assigned_user_names: [
-          'unhcr_cw'
-        ]
-        //created_by: 'unhcr_cw' //Confirm if we set this on update? 
-        //created_by_source: '', // advise on mapping
-      };
-      //console.log('Mapping referral data to Primero');
-      referrals.push(obj);
-    });
 
-    const body = referrals;
+    const body = {
+      progres_interventionnumber: data.progres_interventionnumber,
+      // owned_by: '',
+      // service_type: data.progres_interventiontype2,
+      services_section: [
+        {
+          service_response_day_time: data.progres_interventionstartdate,
+          service_request_external: '', // No data from progres
+          service_referral_notes: data.progres_comments_nonrestrictedstore,
+          service_request_title: data['systemuser.title'],
+          service_request_agency: data['systemuser.progres_partner'],
+          service_request_phone: data['systemuser.mobilephone'],
+          service_request_email: data['systemuser.internalemailaddress'],
+          service_referral_notes: data.progres_interventionbyother, // Reason for referral ?
+          service_response_type: '', // advise on mapping
+        },
+      ],
+      closure_reason: data.progres_comments_nonrestrictedstore,
+      unhcr_individual_no: data['individuals.progres_id'],
+      unhcr_id_no: data['individuals.progres_id_registrationgroupid'],
+      name_first: data['individuals.progres_givenname'],
+      name_middle: data['individuals.progres_middlename'],
+      name_last: data['individuals.progres_familyname'],
+      name_nickname: data['individuals.progres_id_commonyusedname'],
+      date_of_birth: data['individuals.progres_dateofbirth'].split('T')[0],
+      age: calculateAge(new Date(data['individuals.progres_dateofbirth'])),
+      sex: data['individuals.progres_sex'],
+      address_current: '', // advise on mapping
+      telephone_current: data['individuals.progres_primaryphonenumber'],
+      protection_concerns: '', // advise on mapping
+      language: [], // Field on sheet is multi-value
+      status: 'open', // advise on mapping
+      module_id: 'primero-cp', //hardcode default
+      remote: true, //hardcode default
+      id: data.progres_primeroid || data['individuals.progres_id'], // Advise on mapping
+      assigned_user_names: ['unhcr_cw'],
+      //created_by: 'unhcr_cw' //Confirm if we set this on update?
+      //created_by_source: '', // advise on mapping
+    };
+    // console.log('Mapping referral data to Primero');
 
     console.log('data to send to Primero:', body);
 
+    // return state;
     return getCases(
       {
         remote: true,
@@ -77,4 +75,5 @@ each(
         }
       }
     )(state);
-  }));
+  })
+);
