bdfde2ab6e185656ff43c64d3e21be9727bdc563
aleksa-krolls <aleksagk@gmail.com>
2021-05-20 17:31:23 +0300

    job to catch error when flow1 fails

---
+++ jobs/1.dtpReferralFails.js
@@ -0,0 +1,39 @@
+alterState(state => {
+    const error = state.error || '';
+
+    let status = 'failed';
+    for (elt of error)
+        if (String(elt).includes('undefined')) {
+            status = 'not_found';
+        }
+
+    const data = {
+        interventionNumber: state.data.data.progres_interventionnumber,
+        reason: 'Intervention referral is missing fields required for sending to Primero. Please include missing fields and re-send the request.'
+    };
+
+    console.log('Sending error message to DTP...', JSON.stringify(data, null, 2));
+    return http
+        .post({
+            url: urlDTP,
+            data: data,
+            headers: {
+                'Ocp-Apim-Subscription-Key':
+                    configuration['Ocp-Apim-Subscription-Key'],
+            },
+            agentOptions: {
+                key,
+                cert,
+            },
+        })(state)
+        .then(() => {
+            //console.log(JSON.stringify(state.data, null, 2));
+            console.log('Response uploaded to DTP/Progres.');
+            return state;
+        })
+        .catch(error => {
+            let newError = error;
+            newError.config = 'REDACTED';
+            throw newError;
+        });
+});
