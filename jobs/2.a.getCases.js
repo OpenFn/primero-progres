32f3e511ea4786e6286712dd508fb1f80c148339
mamadou <mamadou.lakhassane@gmail.com>
2021-06-03 12:11:12 +0000

    fetch lastRunDateTime as a cursor, #17

--- jobs/2.a.getCases.js
+++ jobs/2.a.getCases.js
@@ -1,47 +1,63 @@
-getCases(
-  {
-    remote: true,
-    assigned_user_names: ['unhcr_cw'],
-    last_updated_at: '2021-06-02T20:00:00.587Z..'
-    // last_updated_at: new Date(
-    //   new Date().toISOString().split('T')[0]
-    // ).toISOString(),
-  },
-  state => {
-    const cases = state.data.filter(
-      data =>
-        data.services_section &&
-        data.services_section.some(
-          serv => serv.service_implementing_agency_individual === 'unhcr_cw'
-        )
-      // data.services_section[0].service_implementing_agency === 'unhcr' //old criteria
-    );
+alterState(state => {
+  console.log('Current cursor value:', state.lastRunDateTime);
+  const manualCursor = '2021-06-02T20:00:00.587Z';
 
-    console.log(cases.length, 'referrals fetched.');
-    console.log('Posting to Inbox...', JSON.stringify(cases, null, 2));
+  return getCases(
+    {
+      remote: true,
+      assigned_user_names: ['unhcr_cw'],
+      last_updated_at: `${state.lastRunDateTime || manualCursor}..`,
+    },
+    state => {
+      const cases = state.data.filter(
+        data =>
+          data.services_section &&
+          data.services_section.some(
+            serv => serv.service_implementing_agency_individual === 'unhcr_cw'
+          )
+        // data.services_section[0].service_implementing_agency === 'unhcr' //old criteria
+      );
 
-    state.cases = cases;
+      console.log(cases.length, 'referrals fetched.');
+      console.log('Posting to Inbox...', JSON.stringify(cases, null, 2));
 
-    const { openfnInboxUrl, xApiKey } = state.configuration;
-    return each(cases, state => {
-      const data = {
-        _json: [state.data],
-      };
-      return http
-        .post({
-          url: openfnInboxUrl,
-          data,
-          headers: { 'x-api-key': xApiKey },
-        })(state)
-        .then(() => {
-          console.log('Case posted to openfn inbox.');
-          return state;
-        })
-        .catch(error => {
-          let newError = error;
-          newError.config = 'REDACTED';
-          throw newError;
-        });
-    })(state);
-  }
-);
+      state.cases = cases;
+
+      const { openfnInboxUrl, xApiKey } = state.configuration;
+      return each(cases, state => {
+        const data = {
+          _json: [state.data],
+        };
+        return http
+          .post({
+            url: openfnInboxUrl,
+            data,
+            headers: { 'x-api-key': xApiKey },
+          })(state)
+          .then(() => {
+            console.log('Case posted to openfn inbox.');
+            return state;
+          })
+          .catch(error => {
+            let newError = error;
+            newError.config = 'REDACTED';
+            throw newError;
+          });
+      })(state);
+    }
+  )(state);
+});
+
+alterState(state => {
+  let lastRunDateTime = state.cases
+    .map(c => c.last_updated_at)
+    .sort((a, b) => new Date(b.date) - new Date(a.date))[0];
+
+  lastRunDateTime =
+    new Date(lastRunDateTime) > new Date()
+      ? lastRunDateTime
+      : new Date().toISOString();
+
+  console.log('New cursor value:', lastRunDateTime);
+  return { ...state, data: {}, references: [], lastRunDateTime };
+});
