027370a28f85c6c176555654862bced49c17ec9d
Aleksa Krolls <aleksagk@gmail.com>
2021-06-03 14:42:30 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/2.b.uploadReferrals.js
+++ jobs/2.b.uploadReferrals.js
@@ -216,10 +216,10 @@ each(
     };
 
     let lang = [];
-    data.language ? data.language.forEach(l => lang.push(languageMap[l])) : data.language;
+    data.language ? data.language.forEach(l => lang.push(languageMap[l])) : lang.push(languageMap['']);
 
     let protection = [];
-    data.protection_concerns ? data.protection_concerns.forEach(pc => protection.push(protectionMap[pc])) : data.protection_concerns;
+    data.protection_concerns ? data.protection_concerns.forEach(pc => protection.push(protectionMap[pc])) : protection.push(protectionMap['']);
 
     //====================================================================================================//
     //==== UPDATE: We now map the Primero Ids for DTP to map to the Progres fields ======================//
