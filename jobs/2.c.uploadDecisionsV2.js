6cf211846df872420b6781feee1b1f1396340993
mamadou <mamadou@openfn.org>
2021-09-29 14:18:58 +0000

    update decision to upload,#30

--- jobs/2.c.uploadDecisionsV2.js
+++ jobs/2.c.uploadDecisionsV2.js
@@ -9,22 +9,19 @@ each(state.data.interventions, state => {
     );
     return referral;
   };
+
   const decision = {
-    module_id: 'primero-cp', //Keep; TO CONFIRM WITH UNHCR
-    case_id, //New mapping for case_id
-    progres_interventionnumber: data.progres_interoperabilityreferralnumber, //New mapping; to confirm if this is same as inter no
-    unhcr_individual_no: data.progres_individualid, //New mapping
-    unhcr_id_no: data.progres_interoperabilityreferralid, //New mapping
     status:
       data.progres_reviewdecision === '125080000'
-        ? 'open' //accepted
+        ? 'accepted'
         : data.progres_reviewdecision === '125080001'
-        ? 'closed'
-        : undefined, //rejected //TODO: Throw error if decision not recognized?
-    //closure_reason: data.progres_comments_nonrestrictedstore, //comment out for now;TO CONFIRM WITH UNHCR
-    //service_referral_notes: data.progres_comments_nonrestrictedstore, //comment out for now; TO CONFIRM WITH UNHCR
-    //service_type: data.progres_interventiontype2, //comment out for now; TO CONFIRM
+        ? 'rejected'
+        : data.progres_reviewdecision,
+    type: 'Referral', //hardcoded
+    // record_id: { record_id }, //different case uuid; not the same as case_id
+    record_type: 'case', //hardcoded
   };
+
   console.log('Decision to send back to Primero:', decision);
 
   return getReferrals({ externalId: 'case_id', id: case_id }, state => {
@@ -36,7 +33,15 @@ each(state.data.interventions, state => {
       return state;
     }
 
-    console.log('Referral to update:', referral);
+    decision['id'] = referral.id;
+    decision['record_id'] = referral.record_id;
+    console.log(
+      `Referral to update: ${JSON.stringify(
+        referral,
+        null,
+        2
+      )} with decision ${JSON.stringify(decision, null, 2)} `
+    );
     return updateReferrals(
       {
         externalId: 'case_id',
@@ -53,27 +58,3 @@ each(state.data.interventions, state => {
     )(state);
   })(state);
 });
-
-// return getCases(
-//   {
-//     remote: true,
-//     case_id,
-//   },
-//   state => {
-//     if (state.data.length === 0) {
-//       throw new Error(
-//         `No case found in Primero for case id ${data.progres_orgreferralid}`
-//         //TODO: Catch error and send notification to DTP
-//       );
-//     } else if (state.data.length === 1) {
-//       const id = state.data[0].id;
-//       console.log(`Updating decision with id: ${id}.`);
-
-//       return getReferrals({ externalId: 'case_id', id: case_id }, state => {
-//         console.log(state.data);
-//         return state;
-//       })(state);
-//     }
-//     return state;
-//   }
-// )(state);
