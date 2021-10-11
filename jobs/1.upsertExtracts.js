487a541c6c27d54245e88b15b5b510e499c713cf
Aleksa Krolls <aleksagk@gmail.com>
2021-10-11 19:42:57 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/1.upsertExtracts.js
+++ jobs/1.upsertExtracts.js
@@ -504,6 +504,7 @@ each(
       case_id: data.progres_primeroid ? data.progres_primeroid : undefined, // Advise on mapping
       owned_by: 'unhcr_cw',
       module_id: 'primeromodule-cp', //hardcode default - to confirm
+      source_identification_referral: 'Humanitarian agencies' //hardcode default - to confirm
       //registration_date: `${today}T00:00:00Z`,
       //associated_user_names: '[unhcr_cw]', //NEEDED?
       //remote: 'true', //NEEDED?
