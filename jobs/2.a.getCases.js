ef370a35c43c4239122861e2d9a1a9cdae42ab4f
Aleksa Krolls <aleksagk@gmail.com>
2021-10-25 16:22:49 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/2.a.getCases.js
+++ jobs/2.a.getCases.js
@@ -17,7 +17,8 @@ fn(state => {
 getCases(
   {
     remote: true,
-    associated_user_names: ['unhcr_cw'],
+    'associated_user_names[0]': ['unhcr_cw'],
+    'associated_user_names[1]': ['unhcr_cw1'],
     last_updated_at: state => `${state.cursor}..`,
   },
   state => ({
