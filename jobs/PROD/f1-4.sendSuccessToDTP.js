f19e61a79fee3d70890e3706da1bd871b4922357
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2021-11-15 17:14:05 -0600

    aissatou@openfn.org's change on OpenFn.org

--- jobs/PROD/f1-4.sendSuccessToDTP.js
+++ jobs/PROD/f1-4.sendSuccessToDTP.js
@@ -1,1 +1,38 @@
-// Your job goes here.
+alterState(state => {
+  const { configuration } = state;
+  const { urlDTP, key, cert } = configuration;
+
+  const primero_user = state.data.owned_by || state.references[1][0].owned_by;
+  const data = {
+    status: 'Pending Acknowledgement',
+    primero_user,
+    case_id: state.references[1][0].case_id,
+    progres_interventionnumber: state.references[0].progres_interventionnumber,
+  };
+
+  console.log(
+    'Sending success message to DTP...',
+    JSON.stringify(data, null, 2)
+  );
+  return http
+    .post({
+      url: urlDTP,
+      data: data,
+      headers: {
+        'Ocp-Apim-Subscription-Key': configuration['Ocp-Apim-Subscription-Key'],
+      },
+      agentOptions: {
+        key,
+        cert,
+      },
+    })(state)
+    .then(() => {
+      console.log('Message sent to DTP/Progres.');
+      return state;
+    })
+    .catch(error => {
+      let newError = error;
+      newError.config = 'REDACTED';
+      throw newError;
+    });
+});
