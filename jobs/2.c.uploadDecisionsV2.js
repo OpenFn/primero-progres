00ae343c97e94862d461417747ba720039e821ad
Aleksa Krolls <aleksagk@gmail.com>
2021-11-22 18:54:05 +0200

    aleksa@openfn.org's change on OpenFn.org

--- jobs/2.c.uploadDecisionsV2.js
+++ jobs/2.c.uploadDecisionsV2.js
@@ -24,7 +24,7 @@ each(state.data.interventions, state => {
     type: 'Referral', //hardcoded
     // record_id: { record_id }, //different case uuid; not the same as case_id
     record_type: 'case', //hardcoded
-    reason_rejected: data.progres_interoperabilityreferralrejectionomment
+    rejected_reason: data.progres_interoperabilityreferralrejectionomment
   };
 
   console.log('Decision to send back to Primero:', decision);
